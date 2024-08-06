c80d74d57bedb528b88490c7275453d9ac911e32
lavie head <nadya.karpenko@gmail.com>
2024-08-06 21:58:12 +0100

    Code

---
+++ src/main/java/ru/netology/page/StartPage.java
@@ -0,0 +1,166 @@
+package ru.netology.page;
+
+import com.codeborne.selenide.Condition;
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import ru.netology.testUtils.Cards;
+
+import java.time.Duration;
+
+import static com.codeborne.selenide.Condition.hidden;
+import static com.codeborne.selenide.Condition.visible;
+import static com.codeborne.selenide.Selectors.byText;
+import static com.codeborne.selenide.Selenide.*;
+
+
+public class StartPage {
+
+    protected String host = System.getProperty("test.host");
+    protected SelenideElement header = $(By.cssSelector("h3"));
+    protected SelenideElement payButton = $(byText("Купить")).parent().parent();
+    protected SelenideElement creditButton = $(byText("Купить в кредит")).parent().parent();
+    protected SelenideElement continueButton = $(byText("Продолжить")).parent().parent();
+    protected SelenideElement cardNumberField = $(byText("Номер карты")).parent();
+    protected SelenideElement monthField = $(byText("Месяц")).parent();
+    protected SelenideElement yearField = $(byText("Год")).parent();
+    protected SelenideElement ownerField = $(byText("Владелец")).parent();
+    protected SelenideElement cvcField = $(byText("CVC/CVV")).parent();
+    protected SelenideElement notificationApproved = $(".notification_status_ok ");
+    protected SelenideElement notificationDeclined = $(".notification_status_error");
+
+    public StartPage() {
+        open(host);
+        header.shouldBe(visible);
+    }
+
+    public void inputData(Cards card) {
+        cardNumberField.$(".input__control").setValue(card.getNumber());
+        monthField.$(".input__control").setValue(card.getMonth());
+        yearField.$(".input__control").setValue(card.getYear());
+        ownerField.$(".input__control").setValue(card.getCardholder());
+        cvcField.$(".input__control").setValue(card.getCvc());
+    }
+
+    public void cleanNumberAndInputNewData(String data) {
+        cardNumberField.$(".input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        cardNumberField.$(".input__control").setValue(data);
+    }
+
+    public void cleanMonthAndInputNewData(String data) {
+        monthField.$(".input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        monthField.$(".input__control").setValue(data);
+    }
+
+    public void cleanYearAndInputNewData(String data) {
+        yearField.$(".input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        yearField.$(".input__control").setValue(data);
+    }
+
+    public void cleanOwnerAndInputNewData(String data) {
+        ownerField.$(".input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        ownerField.$(".input__control").setValue(data);
+    }
+
+    public void cleanCvcAndInputNewData(String data) {
+        cvcField.$(".input__control").sendKeys(Keys.chord(Keys.CONTROL, "a"), Keys.DELETE);
+        cvcField.$(".input__control").setValue(data);
+    }
+
+    public static StartPage payButtonClick() {
+        StartPage payPage = page(StartPage.class);
+        payPage.payButton.click();
+        return payPage;
+    }
+
+    public static StartPage creditPayButtonClick() {
+        StartPage creditPayPage = page(StartPage.class);
+        creditPayPage.creditButton.click();
+        return creditPayPage;
+    }
+
+    public void continueButtonClick() {
+        continueButton.click();
+    }
+
+    public void checkNotificationApprovedVisible() {
+        notificationApproved.shouldBe(visible, Duration.ofMillis(15000));
+    }
+
+    public void checkNotificationDeclinedNotVisible() {
+        notificationDeclined.shouldBe(hidden, Duration.ofMillis(15000));
+    }
+
+    public void checkNotificationDeclinedVisible() {
+        notificationDeclined.shouldBe(visible, Duration.ofMillis(15000));
+    }
+
+    public void checkNotificationApprovedNotVisible() {
+        notificationApproved.shouldBe(hidden, Duration.ofMillis(15000));
+    }
+
+    public void checkCardNumberFormatErrorHave() {
+        cardNumberField.$(".input__sub").shouldHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkCardNumberFormatErrorNotHave() {
+        cardNumberField.$(".input__sub").shouldNotHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkMonthFormatErrorHave() {
+        monthField.$(".input__sub").shouldHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkMonthFormatErrorNotHave() {
+        monthField.$(".input__sub").shouldNotHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkMonthDateErrorHave() {
+        monthField.$(".input__sub").shouldHave(Condition.exactText("Неверно указан срок действия карты"));
+    }
+
+    public void checkMonthDateErrorNotHave() {
+        monthField.$(".input__sub").shouldNotHave(Condition.exactText("Неверно указан срок действия карты"));
+    }
+
+    public void checkYearErrorHave() {
+        yearField.$(".input__sub").shouldHave(Condition.exactText("Неверно указан срок действия карты"));
+    }
+
+    public void checkDateErrorHave() {
+        $(".input__sub").shouldHave(Condition.exactText("Истёк срок действия карты"));
+    }
+
+    public void checkYearFormatErrorHave() {
+        yearField.$(".input__sub").shouldHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkYearFormatErrorNotHave() {
+        yearField.$(".input__sub").shouldNotHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkOwnerErrorHave() {
+        ownerField.$(".input__sub").shouldHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    public void checkOwnerErrorNotHave() {
+        ownerField.$(".input__sub").shouldNotHave(Condition.exactText("Поле обязательно для заполнения"));
+    }
+
+    public void checkOwnerFormatErrorHave() {
+        ownerField.$(".input__sub").shouldHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkOwnerFormatErrorNotHave() {
+        ownerField.$(".input__sub").shouldNotHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkCvcErrorHave() {
+        cvcField.$(".input__sub").shouldHave(Condition.exactText("Неверный формат"));
+    }
+
+    public void checkCvcErrorNotHave() {
+        cvcField.$(".input__sub").shouldNotHave(Condition.exactText("Неверный формат"));
+    }
+
+}
---
+++ src/main/java/ru/netology/test/CashTests.java
@@ -0,0 +1,202 @@
+package ru.netology.test;
+
+import com.codeborne.selenide.logevents.SelenideLogger;
+import io.qameta.allure.selenide.AllureSelenide;
+import org.junit.jupiter.api.*;
+import org.junit.jupiter.params.ParameterizedTest;
+import org.junit.jupiter.params.provider.CsvFileSource;
+import ru.netology.page.StartPage;
+import ru.netology.testUtils.TestDataGenerator;
+import ru.netology.testUtils.TestSQLHelper;
+
+import java.sql.SQLException;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+public class CashTests {
+
+    @BeforeAll
+    static void setUpAll() throws SQLException {
+        TestSQLHelper.cleanTables();
+        SelenideLogger.addListener("allure", new AllureSelenide());
+    }
+
+    @AfterAll
+    static void tearDownAll() {
+        SelenideLogger.removeListener("allure");
+    }
+
+    @AfterEach
+    void cleanTables() throws SQLException {
+        TestSQLHelper.cleanTables();
+    }
+
+    @Test
+    @DisplayName("1. Купить. Успешная оплата разрешённой картой")
+    void shouldConfirmPayWithApprovedCard() {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.continueButtonClick();
+        page.checkNotificationApprovedVisible();
+        assertEquals("APPROVED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+    }
+
+    @Test
+    @DisplayName("2. Купить. Отказ в оплате запрещённой картой")
+    void shouldNotConfirmPayWithDeclinedCard() {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getDeclinedCard());
+        page.continueButtonClick();
+        page.checkNotificationDeclinedVisible();
+        assertEquals("DECLINED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/notExistCardNumber.cvs", numLinesToSkip = 1)
+    @DisplayName("3. Купить. Отказ. Номер карты валиден, но не существует в системе")
+    void shouldNotConfirmPayWithBadCard(String number, String message) {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanNumberAndInputNewData(number);
+        page.continueButtonClick();
+        page.checkNotificationDeclinedVisible();
+        assertEquals("DECLINED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()), message);
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/wrongCardNumber.cvs", numLinesToSkip = 1)
+    @DisplayName("4. Купить. Оплата проходит после исправления номера карты на валидный")
+    void shouldPayProcessAfterRetypeNumCard(String number, String message) {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanNumberAndInputNewData(number);
+        page.continueButtonClick();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()), message);
+        page.checkCardNumberFormatErrorHave();
+        page.cleanNumberAndInputNewData(TestDataGenerator.approvedCardNumber());
+        page.continueButtonClick();
+        page.checkNotificationApprovedVisible();
+        assertEquals("APPROVED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+        page.checkCardNumberFormatErrorNotHave();
+    }
+
+    @Test
+    @DisplayName("5. Купить. Оплата не происходит. Карта просрочена на месяц")
+    void shouldNotPayProcessWithOverdueMonth() {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanMonthAndInputNewData(TestDataGenerator.getShiftedMMFromCurrent(-1));
+        page.cleanYearAndInputNewData(TestDataGenerator.getShiftedYYFromCurrentByMonth(-1));
+        page.continueButtonClick();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+        page.checkDateErrorHave();
+    }
+
+    @Test
+    @DisplayName("6. Купить. Оплата не происходит. Карта просрочена на год")
+    void shouldNotPayProcessWithOverdueYear() {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanMonthAndInputNewData(TestDataGenerator.getShiftedMMFromCurrent(0));
+        page.cleanYearAndInputNewData(TestDataGenerator.getShiftedYYFromCurrent(-1));
+        page.continueButtonClick();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+        page.checkDateErrorHave();
+    }
+
+    @Test
+    @DisplayName("7. Купить. Оплата не происходит. Срок действия карты больше 5 лет")
+    void shouldNotPayProcessWithYearMore5FromCurrent() {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanMonthAndInputNewData(TestDataGenerator.getShiftedMMFromCurrent(0));
+        page.cleanYearAndInputNewData(TestDataGenerator.getShiftedYYFromCurrent(6));
+        page.continueButtonClick();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+        page.checkYearErrorHave();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/wrongMonth.cvs", numLinesToSkip = 1)
+    @DisplayName("8. Купить. Оплата проходит после исправления месяца")
+    void shouldPayProcessWithCorrectionWrongMonth(String month, String message) {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanMonthAndInputNewData(month);
+        page.continueButtonClick();
+        page.checkMonthDateErrorHave();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()), message);
+        page.cleanMonthAndInputNewData("12");
+        page.continueButtonClick();
+        page.checkNotificationApprovedVisible();
+        page.checkMonthDateErrorNotHave();
+        assertEquals("APPROVED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/wrongYear.cvs", numLinesToSkip = 1)
+    @DisplayName("9. Купить. Оплата проходит после исправления года")
+    void shouldPayProcessWithCorrectionWrongYear(String year, String message) {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanYearAndInputNewData(year);
+        page.continueButtonClick();
+        page.checkYearFormatErrorHave();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()), message);
+        page.cleanYearAndInputNewData(TestDataGenerator.getShiftedYYFromCurrent(2));
+        page.continueButtonClick();
+        page.checkNotificationApprovedVisible();
+        page.checkYearFormatErrorNotHave();
+        assertEquals("APPROVED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/wrongCardholderName.cvs", numLinesToSkip = 1)
+    @DisplayName("10. Купить. Оплата происходит после исправления имени владельца")
+    void shouldPayProcessWithCorrectedCardholderName(String owner) {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanOwnerAndInputNewData(owner);
+        page.continueButtonClick();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+        page.checkOwnerFormatErrorHave();
+        page.cleanOwnerAndInputNewData(TestDataGenerator.generateCardholder());
+        page.continueButtonClick();
+        page.checkNotificationApprovedVisible();
+        assertEquals("APPROVED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+        page.checkOwnerFormatErrorNotHave();
+    }
+
+    @ParameterizedTest
+    @CsvFileSource(resources = "/wrongCVV.cvs", numLinesToSkip = 1)
+    @DisplayName("11. Купить. Оплата происходит после исправления CVC/CVV")
+    void shouldPayProcessWithCorrectedCvc(String cvv, String message) {
+        StartPage page = StartPage.payButtonClick();
+        page.inputData(TestDataGenerator.getApprovedCard());
+        page.cleanCvcAndInputNewData(cvv);
+        page.continueButtonClick();
+        page.checkNotificationDeclinedNotVisible();
+        page.checkNotificationApprovedNotVisible();
+        assertEquals("", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()), message);
+        page.checkCvcErrorHave();
+        page.cleanCvcAndInputNewData(TestDataGenerator.generateCvc());
+        page.continueButtonClick();
+        page.checkNotificationApprovedVisible();
+        assertEquals("APPROVED", TestSQLHelper.getOperationStatus(TestSQLHelper.getPayTable()));
+        page.checkCvcErrorNotHave();
+    }
+}
