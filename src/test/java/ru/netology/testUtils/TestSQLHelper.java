a3287a60677b96b1c45930876d2b38b1c2c9533d
lavie head <nadya.karpenko@gmail.com>
2024-09-29 21:17:33 +0100

    Update

--- src/test/java/ru/netology/testUtils/TestSQLHelper.java
+++ src/test/java/ru/netology/testUtils/TestSQLHelper.java
@@ -1,33 +1,42 @@
 package ru.netology.testUtils;
 
+import lombok.AccessLevel;
+import lombok.Getter;
+
 import java.sql.*;
 
 public class TestSQLHelper {
 
-    private static String dbUrl = System.getProperty("db.url"); //для запуска из консоли
-    private static String dbUser = System.getProperty("db.user");
-    private static String dbPass = System.getProperty("db.pass");
+//    private static String dbUrl = System.getProperty("db.url"); //для запуска из консоли
+//    private static String dbUser = System.getProperty("db.user");
+//    private static String dbPass = System.getProperty("db.pass");
+
+//    private static final String dbUrl = "jdbc:postgresql://127.0.0.1:5432/app"; //для запуска из Idea
+    private static final String dbUrl = "jdbc:mysql://185.119.57.164:3306/app"; //для запуска из Idea
+    private static final String dbUser = "app";
+    private static final String dbPass = "pass";
 
     public static String getPayTable() {
         return payTable;
-    }
-
-    public static void setPayTable(String payTable) {
-        TestSQLHelper.payTable = payTable;
     }
 
     public static String getCreditTable() {
         return creditTable;
     }
 
+    @Getter(AccessLevel.NONE)
+    private static String payTable = "payment_entity";
+
+    @Getter(AccessLevel.NONE)
+    private static String creditTable = "credit_request_entity";
+
+    public static void setPayTable(String payTable) {
+        TestSQLHelper.payTable = payTable;
+    }
+
     public static void setCreditTable(String creditTable) {
         TestSQLHelper.creditTable = creditTable;
     }
-
-    private static String payTable = "payment_entity";
-
-    private static String creditTable = "credit_request_entity";
-
 
     public static void cleanTables() throws SQLException {
         String cleanCreditTables = "DELETE FROM credit_request_entity;";
